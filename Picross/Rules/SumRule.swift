388cc652e4e190c831c5c309928b3fa81627fc10
Philipp Brendel <waldrumpus@gmail.com>
2019-05-30 12:33:35 +0200

    add sum rule

---
+++ Picross/Rules/SumRule.swift
@@ -0,0 +1,24 @@
+//
+//  SumRule.swift
+//  Picross
+//
+//  Created by Philipp Brendel on 30.05.19.
+//  Copyright © 2019 Philipp Brendel. All rights reserved.
+//
+
+import Foundation
+
+class SumRule: Rule {
+    override var name: String { return "Sum" }
+    
+    override func apply(to row: [Mark], hints: [Int]) -> [Mark] {
+        let spaceCount = row.filter {$0 != .marked}.count
+        let hintCount = hints.reduce(0, +)
+        
+        guard spaceCount == hintCount else {
+            return row
+        }
+        
+        return row.map {$0 == .unknown ? .chiseled : $0}
+    }
+}
