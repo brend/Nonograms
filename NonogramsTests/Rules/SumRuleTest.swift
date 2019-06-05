23eff1767c99305f4363eab11c2922eadf38f9bf
Philipp Brendel <waldrumpus@gmail.com>
2019-06-05 21:25:07 +0200

    remove tests

---
+++ NonogramsTests/Rules/SumRuleTest.swift
@@ -0,0 +1,18 @@
+//
+//  SumRuleTest.swift
+//  NonogramsTests
+//
+//  Created by Philipp Brendel on 05.06.19.
+//  Copyright © 2019 Philipp Brendel. All rights reserved.
+//
+
+import XCTest
+@testable import Nonograms
+
+class SumRuleTest: XCTestCase {
+
+    let rule = SumRule()
+
+    
+    
+}
