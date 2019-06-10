0e0e58e9d40948df86b6c8d31cefd77845e55dc2
Philipp Brendel <waldrumpus@gmail.com>
2019-06-10 13:57:49 +0200

    fix mouse coordinate calculation

--- Peacross/PuzzleView.swift
+++ Peacross/PuzzleView.swift
@@ -170,7 +170,6 @@ class PuzzleView: NSView {
         
         let hintText = hints.map {String($0)}.joined(separator: "\n") as NSString
         let attrs = [NSAttributedString.Key.font: hintFont as Any]
-        //let textSize = hintText.size(withAttributes: attrs)
         let hintCoords = CGPoint(x: boxArea.minX + boxSize.width * (CGFloat(columnIndex) + 0.3),
                                  y: boxArea.maxY)
         
@@ -200,58 +199,6 @@ class PuzzleView: NSView {
         }
     }
     
-//    func previewNextStep() {
-//        guard phase == .displayIntegration,
-//            let step = step
-//        else { return }
-//
-//        NSColor(deviceRed: 1, green: 0, blue: 0, alpha: 0.3).setFill()
-//
-//        switch step.row {
-//        case let c as ColumnIntegratable:
-//            let colRect = NSRect(x: boxArea.minX + boxSize.width * CGFloat(c.columnIndex), y: boxArea.maxY, width: boxSize.width, height: boxArea.height)
-//            NSBezierPath.fill(colRect)
-//
-//            for (i, mark) in step.after.enumerated() {
-//                switch mark {
-//                case .chiseled:
-//                    NSBezierPath.fill(boxRect(rowIndex: i, columnIndex: c.columnIndex))
-//                case .marked:
-//                    break
-//                default:
-//                    break
-//                }
-//            }
-//
-//            break
-//        case let r as RowIntegratable:
-//            let rowRect = NSRect(x: boxArea.minX, y: boxArea.minY + CGFloat(r.rowIndex) * boxSize.height, width: boxArea.width, height: boxSize.height)
-//
-//            NSBezierPath.fill(rowRect)
-//
-//            for (i, mark) in step.after.enumerated() {
-//                switch mark {
-//                case .chiseled:
-//                    NSColor.green.setFill()
-//                    NSBezierPath.fill(boxRect(rowIndex: r.rowIndex, columnIndex: i))
-//                case .marked:
-//                    let boxRect = NSRect(x: boxArea.minX + boxSize.width * CGFloat(i), y: boxArea.minY + CGFloat(r.rowIndex) * boxSize.height, width: boxSize.width, height: boxSize.height)
-//
-//                    NSColor.red.setFill()
-//                    NSBezierPath.fill(boxRect)
-//                default:
-//                    break
-//                }
-//            }
-//
-//            break
-//        default:
-//            break
-//        }
-//
-//        NSString("preview").draw(at: NSPoint(x: 10, y: 10), withAttributes: nil)
-//    }
-    
     // MARK: - User Interaction
     
     override func mouseDown(with event: NSEvent) {
@@ -268,10 +215,10 @@ class PuzzleView: NSView {
     func coordinates(from event: NSEvent) -> (rowIndex: Int, columnIndex: Int)? {
         guard let size = delegate?.puzzleSize else { return nil }
         
-        let coordsInView = convert(event.locationInWindow, to: self)
+        let coordsInView = self.convert(event.locationInWindow, from: nil)
         let area = boxArea
-        let rowIndex = Int((coordsInView.y - area.minY - boxSize.height / 2) / boxSize.height)
-        let columnIndex = Int((coordsInView.x - area.minX - boxSize.width / 2) / boxSize.width)
+        let rowIndex = Int((coordsInView.y - area.minY) / boxSize.height)
+        let columnIndex = Int((coordsInView.x - area.minX) / boxSize.width)
         
         guard rowIndex >= 0 && rowIndex < size
             && columnIndex >= 0 && columnIndex < size
